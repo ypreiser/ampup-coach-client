0ce20c93033d0299c1933e2721495673e2b5d977
ypreiser <ypreiser@gmail.com>
2024-07-10 12:59:06 +0300

    products

--- src/Pages/Store/StoreSetUp/StoreSetUp.tsx
+++ src/Pages/Store/StoreSetUp/StoreSetUp.tsx
@@ -8,31 +8,33 @@ import storeData from '../../../fakeData/storesData.json'
 const StoreSetUp: React.FC = () => {
     // const storeId = location.pathname.split('/')[2] 
     const store = storeData[0]
-    console.log(store)
-    const {products} = store
+    const { products } = store
 
-  return (
-    <div className={styles.container}>
-      <button className={styles.saveButton}>Save Store</button>
-      <table className={styles.table}>
-        <thead className={styles.thead}>
-          <tr>
-            <th>My cards</th>
-            <th>Price</th>
-            <th>Amount per challenge</th>
-            <th>When available</th>
-            <th>Link/Text</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product, index) => (
-            <ProductCard key={index} quantity={product.quantity} name={product.name} description={product.description} image={product.image} coins={product.coins} daysToExpiry={product.daysToExpiry}  />
-          ))}
-        </tbody>
-      </table>
-      <button className={styles.newCardButton}>➕ New card</button>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>Store Set Up</h1>
+                <button className={styles.saveButton}> ✔️ Save Store</button>
+            </div>
+            <table className={styles.table}>
+                <thead className={styles.thead}>
+                    <tr>
+                        <th>My cards</th>
+                        <th>Price</th>
+                        <th>Amount per challenge</th>
+                        <th>When available</th>
+                        <th>Link/Text</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <ProductCard key={index} quantity={product.quantity} name={product.name} description={product.description} image={product.image} coins={product.coins} daysToExpiry={product.daysToExpiry} />
+                    ))}
+                </tbody>
+            </table>
+            <button className={styles.newCardButton}> + New card</button>
+        </div>
+    );
 };
 
 export default StoreSetUp;
